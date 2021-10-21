18ae46cd19ac97642fd1b8e549f725150b41003e
Attique Ahmed <74119758+AttiqueAhmed@users.noreply.github.com>
2021-10-21 17:46:10 +0600

    Add files via upload

---
+++ PS_2_22_10/PS_2_22_10.java
@@ -0,0 +1,24 @@
+package PS_2_22_10;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PS_2_22_10 extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root =
+            FXMLLoader.load(getClass().getResource("PS_2_22_10.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Draw shapes with Canvas");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
