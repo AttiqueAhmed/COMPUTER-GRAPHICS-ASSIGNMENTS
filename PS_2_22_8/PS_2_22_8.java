1189d1dcbccdbadfd4e3124a3ea0a9e5c7bfd885
Attique Ahmed <74119758+AttiqueAhmed@users.noreply.github.com>
2021-10-21 17:30:46 +0600

    Add files via upload

---
+++ PS_2_22_8/PS_2_22_8.java
@@ -0,0 +1,24 @@
+package PS_2_22_8;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class PS_2_22_8 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("PS_2_22_8.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("PS_2_22_8");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
+
